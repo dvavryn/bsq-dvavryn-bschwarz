156a824e3e8d6e37c93a9758691efde5f66e056c
Dominic Vavryn <dvavryn@c3r5p5.42vienna.com>
2025-02-24 15:06:16 +0100

    a

--- gemeinsam/header.h
+++ gemeinsam/header.h
@@ -6,7 +6,7 @@
 /*   By: dvavryn <dvavryn@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 14:16:04 by dvavryn           #+#    #+#             */
-/*   Updated: 2025/02/24 14:19:59 by dvavryn          ###   ########.fr       */
+/*   Updated: 2025/02/24 14:28:42 by dvavryn          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,10 @@
 # include <unistd.h>
 // exit, malloc, free
 # include <stdlib.h>
+
+/* ILLEGAL!!!! */
+// printf
+# include <stdio.h>
+/* ILLEGAL!!!! */
 
 #endif
