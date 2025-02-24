4559afa38d1405385b89f8f0148014f27695bc14
Tezzarius <mortis@live.at>
2025-02-24 22:28:20 +0100

    split funktions

--- ben/test_matrix.c
+++ ben/test_matrix.c
@@ -1,69 +1,78 @@
 #include <stdio.h>
 
-int	ft_search(int size, char matrix[15][15])
+int	ft_check_area(int size, char matrix[15][15], int k, int l)
 {
 	int i;
 	int j;
-	int k = 0;
-	int l;
-	int count = 0;
 	int check;
 
+	i = 0;
+	check = 0;
+	while (i < size)
+	{
+		j = 0;
+		while (j < size)
+		{
+			if (matrix[i + k][j + l] == 'o')
+				check++;
+			j++;
+		}
+		i++;
+	}
+	return (check == 0);
+}
+
+int	ft_search(int size, char matrix[15][15])
+{
+	int k;
+	int l;
+	int count;
+
+	k = 0;
+	count = 0;
 	while (k <= 15 - size)
 	{
 		l = 0;
 		while (l <= 15 - size)
 		{
-			i = 0;
-			check = 0;
-			while (i < size)
-			{
-				j = 0;
-				while (j < size)
-				{
-					if (matrix[i + k][j + l] == 'o')
-						check++;
-					j++;
-				}
-				i++;			
-			}
-			if (check == 0)
+			if (ft_check_area(size, matrix, k, l))
 				count++;
 			l++;
 		}
 		k++;
 	}
 	printf("Found with size %dx%d: %d\n", size, size, count);
-	return (0);
+	return (count);
 }
 
-int	main()
+int	main(void)
 {
 	char	matrix[15][15] = {
-	{"......o........"},
-	{".....o........."},
-	{".........o....."},
-	{".......o....oo."},
-	{"..............."},
-	{"..............."},
-	{".........o....."},
-	{"..............."},
-	{"..............."},
-	{"..............."},
-	{"o...o.........."},
-	{"o..o....o.....o"},
-	{"..............."},
-	{".............o."},
-	{"....o.........."},
+		"......o........",
+		".....o.........",
+		".........o.....",
+		".......o....oo.",
+		"...............",
+		"...............",
+		".........o.....",
+		"...............",
+		"...............",
+		"...............",
+		"o...o..........",
+		"o..o....o.....o",
+		"...............",
+		".............o.",
+		"....o.........."
 	};
-	int count = 0;
-	int size = 1;
-	
+	int	size;
+	int	total_count;
+
+	size = 1;
+	total_count = 0;
 	while (size <= 15)
 	{
-		count += ft_search(size, matrix);
+		total_count += ft_search(size, matrix);
 		size++;
 	}
-/* 	printf("%d\n", count);
- */	return (0);
+	return (0);
 }
