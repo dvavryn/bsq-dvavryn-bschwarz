fe22262b744d30c775adfb73f9ce81915e864ff0
Dominic Vavryn <dvavryn@c3r5p5.42vienna.com>
2025-02-24 15:41:12 +0100

    a

--- dom/main.c
+++ dom/main.c
@@ -6,7 +6,7 @@
 /*   By: dvavryn <dvavryn@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 13:10:35 by dvavryn           #+#    #+#             */
-/*   Updated: 2025/02/24 14:57:54 by dvavryn          ###   ########.fr       */
+/*   Updated: 2025/02/24 15:39:33 by dvavryn          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,24 +22,40 @@ typedef struct
 
 #define BUF_MAX 1024
 
-int main (void)
+int ft_line_len(char *str);
+
+
+int main (int argc, char **argv)
 {
-	char *filename = "hello.txt";
+	if (argc == 1)
+		printf("USAGE: ./bsq \"FILENAME\"");
+	if (argc != 2)
+		return (-1);
+	char *filename = argv[1];
 	int map = 0;
 	map = open(filename, O_RDONLY);
 	if (map == -1)
 		printf("Map couldnt be opened!\n");
 	printf("Map opened sucessfully!\n");
 
-	char description[BUF_MAX];
+	char buffer[BUF_MAX];
 	int bytes_read;
-	bytes_read = (map, description, BUF_MAX - 1);
+	bytes_read = read(map, buffer, sizeof(buffer) - 1);
 
-	printf("%i bytes read!\ndescription:\n%s\n", bytes_read, description);
+	printf("in map: %i, %i bytes read!\nbuffer:\n%s\n", map, bytes_read, buffer);
+	printf("first line len: %i", ft_line_len(buffer));
 
 
 	close(map);
 	printf("Map closed!\n");
 }
 
+int ft_line_len(char *str)
+{
+	int i;
 
+	i = 0;
+	while (str[i] != '\n')
+		i++;
+	return (i);
+}
--- dom/push.sh
+++ dom/push.sh
@@ -1,3 +1,12 @@
 git add .
+echo "\n"
+git status
+echo "\n"
 git commit -m "a"
+echo "\n"
+git status
+echo "\n"
 git push
+echo "\n"
+git status
+echo "\n"
