1cc8694c40306b3165e4f6b1f53db04f016270fa
Dominic Vavryn <dvavryn@c3r5p5.42vienna.com>
2025-02-24 14:21:00 +0100

    added header

--- dom/header.h
+++ dom/header.h
@@ -6,7 +6,18 @@
 /*   By: dvavryn <dvavryn@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 14:16:04 by dvavryn           #+#    #+#             */
-/*   Updated: 2025/02/24 14:16:05 by dvavryn          ###   ########.fr       */
+/*   Updated: 2025/02/24 14:19:59 by dvavryn          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef HEADER_H
+# define HEADER_H
+
+// open
+# include <fcntl.h>
+// read, close, write
+# include <unistd.h>
+// exit, malloc, free
+# include <stdlib.h>
+
+#endif
